15a263192273917289caac94f80fdc512e93e578
sonickirb <sonickirb@sonicyan11@gmail.com>
2026-06-29 13:14:57 -0500

    wire or

--- Assets/Scripts/Wire.cs
+++ Assets/Scripts/Wire.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Wire : MonoBehaviour
@@ -19,11 +20,15 @@ public class Wire : MonoBehaviour
     {
         line.SetPosition(0, from.transform.Find("Outputs").Find(output.ToString()).position);
         line.SetPosition(1, to.transform.Find("Inputs").Find(input.ToString()).position);
-        line.material = from.inputs[output] ? LogicManager.Instance.nodeOn : LogicManager.Instance.nodeOff;
+        line.material = from.outputs[output] ? LogicManager.Instance.nodeOn : LogicManager.Instance.nodeOff;
     }
 
     public void Tick()
     {
-        to.inputs[input] = from.outputs[output];
+        bool isOn = false;
+        foreach (Wire w in LogicManager.Instance.ConnectedWiresOnOutput(from, output))
+            if (w.from.outputs[output])
+                isOn = true;
+        to.inputs[input] = isOn;
     }
 }
